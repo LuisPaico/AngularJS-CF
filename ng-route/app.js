7aa39748507b09dae6fd279152a00d36fdf48887
Luis Paico <luis@kasfactory.net>
2017-08-24 10:39:06 +0200

    Finish ng-route

--- ng-route/app.js
+++ ng-route/app.js
@@ -10,4 +10,5 @@ angular.module("CustomDirective", ["ngRoute"])
             controller: "RepoController",
             templateUrl: "./templates/repo.html"
         })
-    })
+        .otherwise("/");
+    });
