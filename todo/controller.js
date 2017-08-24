c7ef8b49f2855017d8a47e925e9e79559b783630
Luis Paico <luis@kasfactory.net>
2017-08-24 08:46:06 +0200

    creamos un ToDo simple

---
+++ todo/controller.js
@@ -0,0 +1,27 @@
+angular.module("ToDoList", ["LocalStorageModule"])
+    .controller("ToDoController", function($scope, localStorageService){
+        if(localStorageService.get("angular-todolist")){
+            $scope.todo = localStorageService.get("angular-todolist");
+        }else{
+            $scope.todo = [];
+        };
+        /*
+            {
+                descripcion: 'Terminar el curso Angular',
+                fecha: '15-08-17 2:00pm'
+            }
+        */
+        $scope.$watchCollection('todo',function(newValue,oldValue){
+            localStorageService.set("angular-todolist", $scope.todo);
+        })
+
+        $scope.addActv = function(){
+            $scope.todo.push($scope.newActv);
+            $scope.newActv = {};
+
+        }
+        $scope.clean = function(){
+            $scope.todo = [];
+
+        }
+    })
