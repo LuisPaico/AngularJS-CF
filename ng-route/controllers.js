1d099f65d67a3d308a3a76b431f966966f685f1e
Luis Paico <luis@kasfactory.net>
2017-08-24 08:46:44 +0200

    ng-route

---
+++ ng-route/controllers.js
@@ -0,0 +1,38 @@
+angular.module("CustomDirective")
+
+    .controller('ReposController', function($scope, $http){ 
+        
+                $scope.repos = [];
+        
+                $http.get("https://api.github.com/users/codigofacilito/repos").then(
+                    function(response){
+        
+                        $scope.posts = response.data;
+                        for (var i = response.data.length -1; i >= 0; i--) {
+                            var repo = response.data[i];
+                            $scope.repos.push(repo.name);
+                        }
+                    },
+                    function(err){
+                    }
+                );
+        
+                $scope.optionSelected = function(data){
+                    $scope.$apply(function(){
+                        $scope.main_repo = data;
+                    })
+                }
+            })
+    
+    .controller("RepoController", function($scope, $http, $routeParams){
+        $scope.repo = {};
+        $http.get("https://api.github.com/repos/twitter/"+$routeParams.name).then(
+            function(response){
+
+                $scope.repo = response.data;
+                
+            },
+            function(err){
+            }
+        );
+    })
