b15ceb5488be8f777c047fc71d35089d230599c4
Luis Paico <luis@kasfactory.net>
2017-08-25 10:19:01 +0200

    add ng-resource and create base of users.

--- Final/js/controllers.js
+++ Final/js/controllers.js
@@ -1,4 +1,7 @@
 angular.module("FinalApp")
-.controller("MainController", function($scope){ 
-    
-})
+    .controller("MainController", function($scope, $resource){ 
+        Post = $resource("http://jsonplaceholder.typicode.com/posts/:id", {id: "@id"});
+        User = $resource("http://jsonplaceholder.typicode.com/users/:id", {id: "@id"});
+        $scope.posts = Post.query(); // query() -> GET /posts > Un array de posts
+        $scope.users = User.query();
+    })
